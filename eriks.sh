8778d24180814068d5d3ab4f338deb696b6bdf31
Robert Arkenklo <robert@mrfriday.com>
2016-10-14 10:12:39 +0200

    Better matching

--- eriks.sh
+++ eriks.sh
@@ -2,4 +2,4 @@
 idag="$(LC_ALL=sv_SE.UTF-8 date +%A)"
 
 curl "http://www.eriks.se/gondolen/#meny-lunch" 2>/dev/null | \
-grep -i -m 1 -A 1 $idag | grep -i -v $idag | sed "s:\s*<br />::" | sed 's/^\s*//'
+grep -i -m 1 -A 1 "<strong>$idag" | grep -i -v $idag | sed "s:\s*<br />::" | sed 's/^\s*//'
