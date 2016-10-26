403de10946d2a80aa6ddda2b2a989a44ce51dfd7
Robert Arkenklo <robert@mrfriday.com>
2016-10-26 11:47:20 +0200

    Special case for week 43

--- carinas.sh
+++ carinas.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 idag="$(LC_ALL=sv_SE.UTF-8 date +%A)"
-wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.-$(date +%W).pdf && \
+wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.-$(date +%W).pdf
+if [ $(date +%W) = 43 ]; then
+	wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.$(date +%W)-1.pdf
+fi
 pdftotext /tmp/carinas.pdf - | grep --color=never -A 1 -i $idag | grep -i -v $idag | sed "s/^ *//"
