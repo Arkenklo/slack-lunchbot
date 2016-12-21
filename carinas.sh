753e7e4f354c313b2bbf216e2b2daf472ce5a141
Robert Arkenklo <robert@mrfriday.com>
2016-12-21 11:39:17 +0100

    Super awesome bugfixing

--- carinas.sh
+++ carinas.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 idag="$(LC_ALL=sv_SE.UTF-8 date +%A)"
-wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.-$(date +%W).pdf
-if [ $(date +%W) = 43 ]; then
-	wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.$(date +%W)-1.pdf
-fi
+
+function convert {
 pdftotext /tmp/carinas.pdf - | grep --color=never -A 1 -i $idag | grep -i -v $idag | sed "s/^ *//"
+}
+
+wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.-$(date +%W).pdf
+output=$(convert)
+if [ ! -z "$output" ]; then exit; fi
+
+wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.$(date +%W).pdf
+output=$(convert)
+if [ ! -z "$output" ]; then exit; fi
+
+wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.$(date +%W)-1.pdf
+output=$(convert)
+if [ ! -z "$output" ]; then exit; fi
+
