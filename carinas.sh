184866857955d485440aa5a8d142037f7a181c8e
Robert Arkenklo <robert@mrfriday.com>
2017-01-09 12:19:09 +0100

    Handle low week numbers

--- carinas.sh
+++ carinas.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 idag="$(LC_ALL=sv_SE.UTF-8 date +%A)"
+WEEK=$(date +%W | sed 's/0//')
 
 function convert {
 pdftotext /tmp/carinas.pdf - 2>/dev/null | grep --color=never -A 1 -i $idag | grep -i -v $idag | sed "s/^ *//" 2>/dev/null
@@ -13,12 +14,12 @@ if [ ! -z "$output" ]; then
 fi
 }
 
-wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.-$(date +%W).pdf
+wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.-$WEEK.pdf
 check_pdf
 
-wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.$(date +%W).pdf
+wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.$WEEK.pdf
 check_pdf
 
-wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.$(date +%W)-1.pdf
+wget -q -O /tmp/carinas.pdf http://carinaskok.se/files/meny-v.$WEEK-1.pdf
 check_pdf
 exit 1
