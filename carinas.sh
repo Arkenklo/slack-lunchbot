ed7de610e9d4cbae36c2f00402519005ea115113
Robert Arkenklo <robert@mrfriday.com>
2017-01-09 12:21:29 +0100

    Also handle weeks that end in 0

--- carinas.sh
+++ carinas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 idag="$(LC_ALL=sv_SE.UTF-8 date +%A)"
-WEEK=$(date +%W | sed 's/0//')
+WEEK=$(date +%W | sed 's/^0//')
 
 function convert {
 pdftotext /tmp/carinas.pdf - 2>/dev/null | grep --color=never -A 1 -i $idag | grep -i -v $idag | sed "s/^ *//" 2>/dev/null
